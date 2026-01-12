9e890bc5b9c4658f34de226650175d663d9e6036
Duncan Campbell <campbellduncan12@gmail.com>
2026-01-12 16:30:38 -0600

    Update deps

--- src/main/java/org/squiddev/cctweaks/CCTweaks.java
+++ src/main/java/org/squiddev/cctweaks/CCTweaks.java
@@ -38,7 +38,7 @@ public class CCTweaks {
     public static final String MODID = "cctweaks";
     public static final String NAME = "CCTweaks";
     public static final String VERSION = Tags.VERSION;
-    public static final String DEPENDENCIES = "required-after:ComputerCraft@[1.78.0,);after:CCTurtle;after:ForgeMultipart;after:OpenPeripheralCore;after:OpenPeripheral";
+    public static final String DEPENDENCIES = "required-after:ComputerCraft@[2.0.0,);after:CCTurtle;after:ForgeMultipart;after:OpenPeripheralCore;after:OpenPeripheral";
     public static final Logger logger = LogManager.getLogger();
 
     public static final String ROOT_NAME = "org.squiddev.cctweaks.";
